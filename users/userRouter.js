fdda76f65e5c27b88bf9f44a8f787e9d4008829a
Justin Price <mr.price@Justins-iMac.local>
2020-02-12 21:59:16 -0500

    crud?

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,90 @@
 const express = require('express');
-
 const router = express.Router();
+const Users = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding user',
+      });
+    });
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  const postInfo = { ...req.body, user_id: req.params.id };
+  Posts.insert(postInfo)
+    .then(post => {
+      res.status(210).json(post);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding posts',
+      });
+    });
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the users',
+    });
+  });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  Users.getById(req.params.id)
+  .then(user => {
+      res.status(200).json(user);
+    }) 
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the user',
+      });
+    });
+  });
+
+router.get('/:id/posts', validateUserId, (req, res) => {
+  Posts.getById(req.params.id).then(posts => {
+      res.status(200).json(posts)
+  })
+  .catch(error => {
+      console.log(error);
+      res.status(500).json({errorMessage: "The post could not be retrieved."})
+  });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  Users.remove(req.params.id)
+    .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(error => {
+      res.status(500).json({errorMessage: "The user could not be removed"})
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+      res.status(200).json(user)
+      })
+  .catch(error => {
+      res.status(500).json({errorMessage: "The user information could not be modified."})
+  });
+ 
 });
 
 //custom middleware
